e8f9032d5f15982227e62a981c4b6565afeacf07
Filippo Poma <76634450+Pippoide@users.noreply.github.com>
2025-05-26 02:38:46 +0200

    crete

---
+++ index.js
@@ -0,0 +1,468 @@
+gsap.registerPlugin(Draggable, InertiaPlugin);
+
+
+Draggable.create("#drag1", {
+    type: "x,y",
+    bounds: ".cover",
+    inertia: true,
+    onDragEnd: function () {
+        InertiaPlugin.getVelocity(this.target, "x")
+    },
+    onDrag: function () {
+        const xPosition = this.x;
+        const yPosition = this.y;
+        const rotation = (xPosition + yPosition) % 360;
+        gsap.set(this.target, { rotation: rotation });
+    }
+});
+Draggable.create("#drag2", {
+    type: "x,y",
+    bounds: ".cover",
+    inertia: true,
+    onDragEnd: function () {
+        InertiaPlugin.getVelocity(this.target, "x")
+    },
+    onDrag: function () {
+        const xPosition = this.x;
+        const yPosition = this.y;
+        const rotation = (xPosition + yPosition) % 360;
+        gsap.set(this.target, { rotation: rotation });
+    }
+});
+
+Draggable.create("#drag3", {
+    type: "x,y",
+    bounds: ".cover",
+    inertia: true,
+    onDragEnd: function () {
+        InertiaPlugin.getVelocity(this.target, "x")
+    },
+    onDrag: function () {
+        const xPosition = this.x;
+        const yPosition = this.y;
+        const rotation = (xPosition + yPosition) % 360;
+        gsap.set(this.target, { rotation: rotation });
+    }
+});
+Draggable.create("#drag4", {
+    type: "x,y",
+    bounds: ".cover",
+    inertia: true,
+    onDragEnd: function () {
+        InertiaPlugin.getVelocity(this.target, "x")
+    },
+    onDrag: function () {
+        const xPosition = this.x;
+        const yPosition = this.y;
+        const rotation = (xPosition + yPosition) % 360;
+        gsap.set(this.target, { rotation: rotation });
+    }
+});
+Draggable.create("#drag5", {
+    type: "x,y",
+    bounds: ".cover",
+    inertia: true,
+    onDragEnd: function () {
+        InertiaPlugin.getVelocity(this.target, "x")
+    },
+    onDrag: function () {
+        const xPosition = this.x;
+        const yPosition = this.y;
+        const rotation = (xPosition + yPosition) % 360;
+        gsap.set(this.target, { rotation: rotation });
+    }
+});
+Draggable.create("#drag6", {
+    type: "x,y",
+    bounds: ".cover",
+    inertia: true,
+    onDragEnd: function () {
+        InertiaPlugin.getVelocity(this.target, "x")
+    },
+    onDrag: function () {
+        const xPosition = this.x;
+        const yPosition = this.y;
+        const rotation = (xPosition + yPosition) % 360;
+        gsap.set(this.target, { rotation: rotation });
+    }
+});
+Draggable.create("#drag7", {
+    type: "x,y",
+    bounds: ".cover",
+    inertia: true,
+    onDragEnd: function () {
+        InertiaPlugin.getVelocity(this.target, "x")
+    },
+    onDrag: function () {
+        const xPosition = this.x;
+        const yPosition = this.y;
+        const rotation = (xPosition + yPosition) % 360;
+        gsap.set(this.target, { rotation: rotation });
+    }
+});
+Draggable.create("#drag8", {
+    type: "x,y",
+    bounds: ".cover",
+    inertia: true,
+    onDragEnd: function () {
+        InertiaPlugin.getVelocity(this.target, "x")
+    },
+    onDrag: function () {
+        const xPosition = this.x;
+        const yPosition = this.y;
+        const rotation = (xPosition + yPosition) % 360;
+        gsap.set(this.target, { rotation: rotation });
+    }
+});
+Draggable.create("#drag9", {
+    type: "x,y",
+    bounds: ".cover",
+    inertia: true,
+    onDragEnd: function () {
+        InertiaPlugin.getVelocity(this.target, "x")
+    },
+    onDrag: function () {
+        const xPosition = this.x;
+        const yPosition = this.y;
+        const rotation = (xPosition + yPosition) % 360;
+        gsap.set(this.target, { rotation: rotation });
+    }
+});
+Draggable.create("#drag10", {
+    type: "x,y",
+    bounds: ".cover",
+    inertia: true,
+    onDragEnd: function () {
+        InertiaPlugin.getVelocity(this.target, "x")
+    },
+    onDrag: function () {
+        const xPosition = this.x;
+        const yPosition = this.y;
+        const rotation = (xPosition + yPosition) % 360;
+        gsap.set(this.target, { rotation: rotation });
+    }
+});
+Draggable.create("#drag11", {
+    type: "x,y",
+    bounds: ".cover",
+    inertia: true,
+    onDragEnd: function () {
+        InertiaPlugin.getVelocity(this.target, "x")
+    },
+    onDrag: function () {
+        const xPosition = this.x;
+        const yPosition = this.y;
+        const rotation = (xPosition + yPosition) % 360;
+        gsap.set(this.target, { rotation: rotation });
+    }
+});
+Draggable.create("#drag12", {
+    type: "x,y",
+    bounds: ".cover",
+    inertia: true,
+    onDragEnd: function () {
+        InertiaPlugin.getVelocity(this.target, "x")
+    },
+    onDrag: function () {
+        const xPosition = this.x;
+        const yPosition = this.y;
+        const rotation = (xPosition + yPosition) % 360;
+        gsap.set(this.target, { rotation: rotation });
+    }
+});
+Draggable.create("#drag13", {
+    type: "x,y",
+    bounds: ".cover",
+    inertia: true,
+    onDragEnd: function () {
+        InertiaPlugin.getVelocity(this.target, "x")
+    },
+    onDrag: function () {
+        const xPosition = this.x;
+        const yPosition = this.y;
+        const rotation = (xPosition + yPosition) % 360;
+        gsap.set(this.target, { rotation: rotation });
+    }
+});
+Draggable.create("#drag14", {
+    type: "x,y",
+    bounds: ".cover",
+    inertia: true,
+    onDragEnd: function () {
+        InertiaPlugin.getVelocity(this.target, "x")
+    },
+    onDrag: function () {
+        const xPosition = this.x;
+        const yPosition = this.y;
+        const rotation = (xPosition + yPosition) % 360;
+        gsap.set(this.target, { rotation: rotation });
+    }
+});
+Draggable.create("#drag15", {
+    type: "x,y",
+    bounds: ".cover",
+    inertia: true,
+    onDragEnd: function () {
+        InertiaPlugin.getVelocity(this.target, "x")
+    },
+    onDrag: function () {
+        const xPosition = this.x;
+        const yPosition = this.y;
+        const rotation = (xPosition + yPosition) % 360;
+        gsap.set(this.target, { rotation: rotation });
+    }
+});
+Draggable.create("#drag16", {
+    type: "x,y",
+    bounds: ".cover",
+    inertia: true,
+    onDragEnd: function () {
+        InertiaPlugin.getVelocity(this.target, "x")
+    },
+    onDrag: function () {
+        const xPosition = this.x;
+        const yPosition = this.y;
+        const rotation = (xPosition + yPosition) % 360;
+        gsap.set(this.target, { rotation: rotation });
+    }
+});
+
+Draggable.create("#drag18", {
+    type: "x,y",
+    bounds: ".cover",
+    inertia: true,
+    onDragEnd: function () {
+        InertiaPlugin.getVelocity(this.target, "x")
+    },
+    onDrag: function () {
+        const xPosition = this.x;
+        const yPosition = this.y;
+        const rotation = (xPosition + yPosition) % 360;
+        gsap.set(this.target, { rotation: rotation });
+    }
+});
+Draggable.create("#drag1", {
+    type: "x,y",
+    bounds: ".cover",
+    inertia: true,
+    onDragEnd: function () {
+        InertiaPlugin.getVelocity(this.target, "x")
+    },
+    onDrag: function () {
+        const xPosition = this.x;
+        const yPosition = this.y;
+        const rotation = (xPosition + yPosition) % 360;
+        gsap.set(this.target, { rotation: rotation });
+    }
+});
+Draggable.create("#drag19", {
+    type: "x,y",
+    bounds: ".cover",
+    inertia: true,
+    onDragEnd: function () {
+        InertiaPlugin.getVelocity(this.target, "x")
+    },
+    onDrag: function () {
+        const xPosition = this.x;
+        const yPosition = this.y;
+        const rotation = (xPosition + yPosition) % 360;
+        gsap.set(this.target, { rotation: rotation });
+    }
+});
+Draggable.create("#drag20", {
+    type: "x,y",
+    bounds: ".cover",
+    inertia: true,
+    onDragEnd: function () {
+        InertiaPlugin.getVelocity(this.target, "x")
+    },
+    onDrag: function () {
+        const xPosition = this.x;
+        const yPosition = this.y;
+        const rotation = (xPosition + yPosition) % 360;
+        gsap.set(this.target, { rotation: rotation });
+    }
+});
+Draggable.create("#drag21", {
+    type: "x,y",
+    bounds: ".cover",
+    inertia: true,
+    onDragEnd: function () {
+        InertiaPlugin.getVelocity(this.target, "x")
+    },
+    onDrag: function () {
+        const xPosition = this.x;
+        const yPosition = this.y;
+        const rotation = (xPosition + yPosition) % 360;
+        gsap.set(this.target, { rotation: rotation });
+    }
+});
+Draggable.create("#drag22", {
+    type: "x,y",
+    bounds: ".cover",
+    inertia: true,
+    onDragEnd: function () {
+        InertiaPlugin.getVelocity(this.target, "x")
+    },
+    onDrag: function () {
+        const xPosition = this.x;
+        const yPosition = this.y;
+        const rotation = (xPosition + yPosition) % 360;
+        gsap.set(this.target, { rotation: rotation });
+    }
+});
+Draggable.create("#drag23", {
+    type: "x,y",
+    bounds: ".cover",
+    inertia: true,
+    onDragEnd: function () {
+        InertiaPlugin.getVelocity(this.target, "x")
+    },
+    onDrag: function () {
+        const xPosition = this.x;
+        const yPosition = this.y;
+        const rotation = (xPosition + yPosition) % 360;
+        gsap.set(this.target, { rotation: rotation });
+    }
+});
+Draggable.create("#drag24", {
+    type: "x,y",
+    bounds: ".cover",
+    inertia: true,
+    onDragEnd: function () {
+        InertiaPlugin.getVelocity(this.target, "x")
+    },
+    onDrag: function () {
+        const xPosition = this.x;
+        const yPosition = this.y;
+        const rotation = (xPosition + yPosition) % 360;
+        gsap.set(this.target, { rotation: rotation });
+    }
+});
+Draggable.create("#drag25", {
+    type: "x,y",
+    bounds: ".cover",
+    inertia: true,
+    onDragEnd: function () {
+        InertiaPlugin.getVelocity(this.target, "x")
+    },
+    onDrag: function () {
+        const xPosition = this.x;
+        const yPosition = this.y;
+        const rotation = (xPosition + yPosition) % 360;
+        gsap.set(this.target, { rotation: rotation });
+    }
+});
+Draggable.create("#drag26", {
+    type: "x,y",
+    bounds: ".cover",
+    inertia: true,
+    onDragEnd: function () {
+        InertiaPlugin.getVelocity(this.target, "x")
+    },
+    onDrag: function () {
+        const xPosition = this.x;
+        const yPosition = this.y;
+        const rotation = (xPosition + yPosition) % 360;
+        gsap.set(this.target, { rotation: rotation });
+    }
+});
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+let iteration = 0; // gets iterated when we scroll all the way to the end or start and wraps around - allows us to smoothly continue the playhead scrubbing in the correct direction.
+
+const spacing = 0.1,    // spacing of the cards (stagger)
+    snap = gsap.utils.snap(spacing), // we'll use this to snap the playhead on the seamlessLoop
+    cards = gsap.utils.toArray('.cards li'),
+    seamlessLoop = buildEffect(cards, spacing),
+    scrub = gsap.to(seamlessLoop, { // we reuse this tween to smoothly scrub the playhead on the seamlessLoop
+        totalTime: 0,
+        duration: 0.5,
+        ease: "power3",
+        paused: true
+    }),
+    trigger = ScrollTrigger.create({
+        trigger: '.gallery',
+        start: 'top top',
+        end: () => '+=3000',
+        pin: ".gallery",
+        onUpdate(self) {
+            if (self.progress < 1 || self.progress > 0) {
+                scrub.vars.totalTime = snap(
+                    self.progress * seamlessLoop.duration()
+                );
+                scrub.invalidate().restart();
+            }
+        },
+
+    });
+
+function wrapForward(trigger) { // when the ScrollTrigger reaches the end, loop back to the beginning seamlessly
+    iteration++;
+    trigger.wrapping = true;
+    trigger.scroll(trigger.start + 1);
+}
+
+function wrapBackward(trigger) { // when the ScrollTrigger reaches the start again (in reverse), loop back to the end seamlessly
+    iteration--;
+    if (iteration < 0) { // to keep the playhead from stopping at the beginning, we jump ahead 10 iterations
+        iteration = 9;
+        seamlessLoop.totalTime(seamlessLoop.totalTime() + seamlessLoop.duration() * 10);
+        scrub.pause(); // otherwise it may update the totalTime right before the trigger updates, making the starting value different than what we just set above. 
+    }
+    trigger.wrapping = true;
+    trigger.scroll(trigger.end - 1);
+}
+
+
+function calculateVisibleItems(length) {
+    /*if(length>7)
+      return 2;
+    if(length<5)
+      return 2;*/
+    return 1; //return 1 ovvero il numero di libri visibili
+}
+
+function buildEffect(items, spacing) {
+    let
+        cardsVisible = calculateVisibleItems(items.length), // atleast +1 card should be there to perform this transition for 3 there should be 4 cards
+        cardDuration = (Math.ceil(cardsVisible / 2)) * spacing, //according to visible number of cards, we will decide in what progress cards should go 0-1 opacity or 1-0 opacity. e.g if 3 should be visible then - 0.0, 0.1 , 0.2 so at 0.2 it should complete 
+        startTime = items.length * spacing + cardDuration,
+        loopTime = startTime + (items.length * spacing) - spacing,
+        rawSequence = gsap.timeline({ paused: true }),
+        seamlessLoop = gsap.timeline({
+            paused: true,
+            repeat: -1,
+            onRepeat() {
+                this._time === this._dur && (this._tTime += this._dur - 0.01);
+            }
+        }),
+        time = 0,
+        i, index, item;
+
+    // set initial state of items
+    gsap.set(items, { xPercent: 200, opacity: 0, scale: 0 });
+    //total items should be double of length--it will start on 1st item of second instance of list
+    //there should be extra cards visible on end of 2nd instance of list to make it look like it is in loop
+    for (i = 0; i < (items.length * 2 + cardsVisible); i++) {
+        index = i % items.length;
+        item = items[index];
+        time = i * spacing;
+        //yoyo & repeat is to make 0-1-0 effect for scale and opacity in this case/
+        rawSequence.fromTo(item, { scale: 1, opacity: 0 }, { scale: 1, opacity: 1, zIndex: 100, duration: cardDuration, repeat: 1, yoyo: true, ease: "power1.in", immediateRender: false }, time)
+            //distance between cards -- it needs to run once only while above should be run twice. 0-1-0 with 400 - 0 - -400 or vice versa
+            .fromTo(item, { xPercent: 400 }, { xPercent: -400, duration: cardDuration * 2, ease: "none", immediateRender: false }, time);
+    }
+
+    rawSequence.time(startTime);
+
+    seamlessLoop.to(rawSequence, {
+        time: loopTime,
+        duration: (loopTime - startTime),
+        ease: "none"
+    })
+    return seamlessLoop;
+}
+
